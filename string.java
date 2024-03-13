d668b9ad68e32e09e2d9fe6b3434e68e64c8e23d
SravaniDandu7 <147130492+SravaniDandu7@users.noreply.github.com>
2024-03-13 18:15:30 +0530

    Update string.java

--- string.java
+++ string.java
@@ -2,8 +2,15 @@
 public class string  {
     public static void main(String[] args) {
         String text = "type here to search";
-        for (int i = 1; i < text.length(); i += 2) {
-            System.out.print(text.charAt(i));
+        StringBuilder oddChars = new StringBuilder();
+        text = text.replaceAll("\\s", "");
+        for (int i = 0; i < text.length(); i++) 
+        {
+            if (i % 2 == 0)
+            {
+                oddChars.append(text.charAt(i));
+            }
         }
+        System.out.println(oddChars.toString());
     }
-}
+  }
